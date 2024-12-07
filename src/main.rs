6fde074c8549735521e6c88d7de0644d752db710
Erica Stith <rexlunae@gmail.com>
2024-12-07 14:42:19 -0800

    Serve frontend from backed.

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use salvo::prelude::*;
+use salvo::serve_static::StaticDir;
+
+use log::{info, debug};
+
+#[handler]
+async fn hello() -> &'static str {
+    "Hello World"
+}
+
+#[tokio::main]
+async fn main() {
+    let bind = "127.0.0.1:5800";
+    tracing_subscriber::fmt().init();
+
+    let router = Router::with_path("<**path>").get(
+        StaticDir::new([
+            "frontend/dist"
+        ])
+        .defaults("index.html")
+        .auto_list(true),
+    );
+
+    //let router = Router::new().get(hello);
+    info!("Binding on {}", bind);
+    let acceptor = TcpListener::new(bind).bind().await;
+    Server::new(acceptor).serve(router).await;
+}
